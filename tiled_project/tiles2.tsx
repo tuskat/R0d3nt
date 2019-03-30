2afc1467c6ad207bc82254126b33c50d828cc93f
Tuskat <cedric.marcellin@gmail.com>
2019-03-31 01:48:36 +0800

    ALPHA add Tiled project to git

---
+++ tiled_project/tiles2.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.03.13" name="tiles2" tilewidth="32" tileheight="32" tilecount="10" columns="0">
+ <tile id="0">
+  <image width="32" height="32" source="D:/Workspace/project-rodent/assets/gfx/floor.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="D:/Workspace/project-rodent/assets/gfx/wall.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="D:/Workspace/project-rodent/assets/gfx/top.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="D:/Workspace/project-rodent/assets/gfx/bottom.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="D:/Workspace/project-rodent/assets/gfx/player.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="D:/Workspace/project-rodent/assets/gfx/spawn.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="D:/Workspace/project-rodent/assets/gfx/boss.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="D:/Workspace/project-rodent/assets/gfx/flag.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="D:/Workspace/project-rodent/assets/gfx/ball.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="D:/Workspace/project-rodent/assets/gfx/light.png"/>
+ </tile>
+</tileset>
